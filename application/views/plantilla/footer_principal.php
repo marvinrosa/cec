8bcd5a0796b1950d7cd6930f9d2d1098e438b50d
Marvin <marvin.fortin@outlook.com>
2018-05-03 07:16:11 -0600

    Base Frontend

---
+++ application/views/plantilla/footer_principal.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!-- Cierre Wrapper -->
+</div>
+<!-- JS -->
+  <script language="javaScript"  type="text/javascript" src="<?php echo base_url(); ?>js/jquery.min.js">
+  </script>
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+          integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+          crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.3/js/adminlte.js"></script>
+
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-show-password/1.0.3/bootstrap-show-password.min.js"></script>
+
+
+  <script src="<?php echo base_url(); ?>js/popper.min.js"></script>
+  <script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
+  <script src="<?php echo base_url(); ?>js/jquery.easing.min.js"></script>
+  <script src="<?php echo base_url(); ?>js/owl.carousel.min.js"></script>
+  <script src="<?php echo base_url(); ?>js/wow.min.js"></script> 
+  <script src="<?php echo base_url(); ?>js/app.js"></script>
+  <script src="<?php echo base_url(); ?>js/tether.min.js"></script>
+  <script src="<?php echo base_url(); ?>js/smooth-scroll.js"></script>
+  <script src="<?php echo base_url(); ?>js/script-dropdown.min.js"></script>
+  <script src="<?php echo base_url(); ?>js/bootstrap-carousel-swipe.js"></script>
+  <script src="<?php echo base_url(); ?>js/jquery.touch-swipe.min.js"></script>
+  <script src="<?php echo base_url(); ?>js/script-theme.js"></script>
+  <script src="<?php echo base_url(); ?>js/script-slider-video.js"></script>
+
+
+      
+    
+
+</body>
+</html>
---
+++ application/views/plantilla/frontend/head_css.php
@@ -0,0 +1,27 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>css/estilos.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>css/owl.carousel.min.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>css/animate.min.css">
+
+  	<link rel="stylesheet" href="<?php echo base_url();?>css/app.css">
+
+	<link rel="stylesheet" href="<?php echo base_url();?>css/mobirise-icons/mobirise-icons.css">
+
+	  <link rel="stylesheet" href="<?php echo base_url();?>css/tether.min.css">
+	
+	  <link rel="stylesheet" href="<?php echo base_url();?>css/bootstrap-grid.min.css">
+	  <link rel="stylesheet" href="<?php echo base_url();?>css/bootstrap-reboot.min.css">
+	  
+	  <link rel="stylesheet" href="<?php echo base_url();?>css/style-dropdown.css">
+	  <link rel="stylesheet" href="<?php echo base_url();?>css/style-theme.css">
+	  <link rel="stylesheet" href="<?php echo base_url();?>css/mbr-additional.css" type="text/css">
+  
+
+<!-- -->
+ <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+
+<body>
